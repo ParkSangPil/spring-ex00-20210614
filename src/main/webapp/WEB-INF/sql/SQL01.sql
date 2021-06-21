4310e10199c0897273c31186b954b3936d8b7213
kevin <kevin01123@naver.com>
2021-06-21 17:53:35 +0900

    ㅂ

---
+++ src/main/webapp/WEB-INF/sql/SQL01.sql
@@ -0,0 +1,11 @@
+Create DATABASE spr1;
+use spr1;
+
+CREATE table tbl_board (
+	bno INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) not NULL,
+    content VARCHAR(2000) not null,
+    writer VARCHAR(50) not null,
+    regdata TIMESTAMP DEFAULT NOW(),
+    updatedate TIMESTAMP DEFAULT NOW()
+);
