258e95604f851e97ca1a01b02b08d59c29ed3ef8
kevin <kevin01123@naver.com>
2021-07-15 15:41:33 +0900

    0715 종료

--- src/main/webapp/WEB-INF/sql/SQL09.sql
+++ src/main/webapp/WEB-INF/sql/SQL09.sql
@@ -13,6 +13,7 @@ SELECT * FROM tbl_board
 ORDER BY bno desc;
 desc tbl_member;
 SELECT * FROM tbl_member;
+SELECT * FROM tbl_member_auth;
 
 DELETE from tbl_member where userid = '';
 
@@ -35,9 +36,44 @@ add FOREIGN KEY (replyer) REFERENCES tbl_member(userid);
 
 DESC tbl_board;
 desc tbl_reply;
+SELECT * FROM tbl_board_file;
+desc tbl_board_file;
+SELECT * FROM user_review_file;
 
+DROP table user;
+drop table b2buser;
 
+CREATE TABLE user(
+	bno int PRIMARY	KEY AUTO_INCREMENT,
+    userid VARCHAR(50) not null,
+    userpw VARCHAR(500) not null,
+    userName VARCHAR(50) not null,
+    userAddress VARCHAR(50) not null,
+    birthDate TIMESTAMP DEFAULT now(),
+    userPhonenum int(50) not null,
+    userEmail VARCHAR(50) not null,
+    userSex int(5) not null,
+    userNickname VARCHAR(50) not null
+);
 
+CREATE TABLE b2buser(
+	bno int PRIMARY	KEY AUTO_INCREMENT,
+    userid VARCHAR(50) not null,
+    userpw VARCHAR(500) not null,
+    userName VARCHAR(50) not null,
+    userAddress VARCHAR(50) not null,
+    birthDate TIMESTAMP DEFAULT now(),
+    userPhonenum int(50) not null,
+    userEmail VARCHAR(50) not null,
+    userSex int(5) not null,
+    userNickname VARCHAR(50) not null
+);
+
+SELECT * FROM user;
+SELECT * FROm b2buser;
+
+delete from user;
+delete from b2buser;
 
 
 
