105abdd2dd96bb3ea6cf21c705026760726426d6
kevin <kevin01123@naver.com>
2021-07-15 15:15:16 +0900

    0715 중간

--- src/main/java/org/zerock/security/domain/CustomUser.java
+++ src/main/java/org/zerock/security/domain/CustomUser.java
@@ -1,14 +1,11 @@
 package org.zerock.security.domain;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.zerock.domain.AuthVO;
 import org.zerock.domain.MemberVO;
 
 import lombok.Getter;
